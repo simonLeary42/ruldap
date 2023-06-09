ff5b2e12562c350b5106b952bf1971a472c39140
Cecilia Lau <cecilialau6776@gmail.com>
2023-06-09 14:45:18 -0400

    Change backend to only call on LDAP search once

--- coldfront_plugin_ru_ldap/backend.py
+++ coldfront_plugin_ru_ldap/backend.py
@@ -9,26 +9,29 @@ logger = logging.getLogger(__name__)
 class LDAPRemoteUserBackend(RemoteUserBackend):
     create_unknown_user = False
 
+    def __init__(self):
+        super().__init__()
+
     def authenticate(self, request, remote_user):
         if not remote_user:
-            return 
+            return None
         username = self.clean_username(remote_user)
         search = LDAPUserSearch(username, "username_only")
-        user_dict = search.search_a_user(username, "username_only")
-        if len(user_dict) == 0:
+        self.user_dict = search.search_a_user(username, "username_only")
+        if len(self.user_dict) == 0:
             # LDAP doesn't have this user
             return None
         else:
             self.create_unknown_user = True
-            return super().authenticate(request, remote_user)
+        return super().authenticate(request, remote_user)
 
     def clean_username(self, username):
         return username.split("@")[0]
 
     def configure_user(self, request, user, created=True):
-        search = LDAPUserSearch(user.username, "username_only")
-        user_dict = search.search_a_user(user.username, "username_only")
-        user.first_name = user_dict["first_name"]
-        user.last_name = user_dict["last_name"]
-        user.email = user_dict["email"]
+        ud = self.user_dict[0]
+        logger.debug(ud)
+        user.first_name = ud["first_name"]
+        user.last_name = ud["last_name"]
+        user.email = ud["email"]
         return user
