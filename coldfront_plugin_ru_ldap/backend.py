1e6871638c9b2460b952ea146c459fd5a8c2a0ba
Cecilia Lau <cecilialau6776@gmail.com>
2023-06-09 09:16:25 -0400

    Removed unnecessary files

--- coldfront_plugin_ru_ldap/backend.py
+++ coldfront_plugin_ru_ldap/backend.py
@@ -1,19 +1,19 @@
 from coldfront.plugins.ldap_user_search.utils import LDAPUserSearch
 from django.contrib.auth.backends import RemoteUserBackend
-from django.contrib.auth import get_user_model
 
 import logging
 
 logger = logging.getLogger(__name__)
 
+
 class LDAPRemoteUserBackend(RemoteUserBackend):
     def clean_username(self, username):
         return username.split("@")[0]
+
     def configure_user(self, request, user, created=True):
         search = LDAPUserSearch(user.username, "username_only")
         user_dict = search.search_a_user(user.username, "username_only")[0]
-        user["username"] = user_dict["username"]
-        user["first_name"] = user_dict["first_name"]
-        user["last_name"] = user_dict["last_name"]
-        user["email"] = user_dict["email"]
+        user.first_name = user_dict["first_name"]
+        user.last_name = user_dict["last_name"]
+        user.email = user_dict["email"]
         return user
--- coldfront_plugin_ru_ldap/middleware.py
+++ coldfront_plugin_ru_ldap/middleware.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.middleware import RemoteUserMiddleware
 from coldfront.core.utils.common import import_from_settings
 
+
 class CustomHeaderMiddleware(RemoteUserMiddleware):
     header = import_from_settings("RULDAP_CUSTOM_HEADER", "REMOTE_USER")
