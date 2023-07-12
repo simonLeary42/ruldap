6df096ebf9d804a71c7e560bba0ffc750c59045b
Cecilia Lau <cecilialau6776@gmail.com>
2023-07-12 14:14:29 -0400

    Update to search for email

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ruldap - Remote User LDAP
 
-A [ColdFront](https://coldfront.readthedocs.io/en/latest/) plugin that provides `LDAPRemoteUserBackend` that will configure a user based on information pulled from LDAP. If a user doesn't exist in LDAP, the user will **not** be created.
+A [ColdFront](https://coldfront.readthedocs.io/en/latest/) plugin that provides `LDAPRemoteUserBackend` that will configure a user based on information pulled from LDAP. If a user doesn't exist in LDAP, the user will **not** be created. 
 
 ## Installation
 If you're using a virtual environment (following ColdFront's deployment instructions should have you make and use a virtual environment), make sure you're in the virutal environment first.
@@ -21,4 +21,5 @@ AUTHENTICATION_BACKENDS += [
 If you wish to define a custom header, append `coldfront_plugin_ru_ldap.middleware.CustomHeaderMiddleware` to `MIDDLEWARE` instead and set `RULDAP_CUSTOM_HEADER="HTTP_MY_CUSTOM_HEADER"`. **This value can be spoofed.** Be careful and read the [warning](https://docs.djangoproject.com/en/4.2/howto/auth-remote-user/#configuration) from Django's docs. 
 
 ## Details
-`ruldap` uses the [`CombinedUserSearch`](https://github.com/ubccr/coldfront/blob/v1.1.4/coldfront/core/user/utils.py#L74) class built in to ColdFront that uses the classes in `ADDITIONAL_USER_SEARCH_CLASSES`. This means that if you add additional user search classes, it will attempt to use those classes. For example, if you enable the [`ldap_user_search` plugin](https://github.com/ubccr/coldfront/tree/v1.1.4/coldfront/plugins/ldap_user_search), `ruldap` will use `ldap_user_search` to find users.
+~~`ruldap` uses the [`CombinedUserSearch`](https://github.com/ubccr/coldfront/blob/v1.1.4/coldfront/core/user/utils.py#L74) class built in to ColdFront that uses the classes in `ADDITIONAL_USER_SEARCH_CLASSES`. This means that if you add additional user search classes, it will attempt to use those classes. For example, if you enable the [`ldap_user_search` plugin](https://github.com/ubccr/coldfront/tree/v1.1.4/coldfront/plugins/ldap_user_search), `ruldap` will use `ldap_user_search` to find users.~~
+`REMOTE_USER` expects an email, and will check against that.
--- coldfront_plugin_ru_ldap/backend.py
+++ coldfront_plugin_ru_ldap/backend.py
@@ -1,9 +1,12 @@
-from coldfront.core.user.utils import CombinedUserSearch
+from coldfront_plugin_ldap_custom_mapping.utils import LDAPCustomMapping
+from coldfront.core.utils.common import import_from_settings
 from django.contrib.auth.backends import RemoteUserBackend
+from django.contrib.auth import get_user_model
 
 import logging
 
 logger = logging.getLogger(__name__)
+UserModel = get_user_model()
 
 
 class LDAPRemoteUserBackend(RemoteUserBackend):
@@ -15,23 +18,39 @@ class LDAPRemoteUserBackend(RemoteUserBackend):
     def authenticate(self, request, remote_user):
         if not remote_user:
             return None
-        username = self.clean_username(remote_user)
-        search = CombinedUserSearch(username, "username_only")
-        self.user_dict = search.search()["matches"]
+        email = self.clean_username(remote_user)
+        search = LDAPCustomMapping(email, "email")
+        search_results = search.search_a_user(email, "email")
+        self.user_dict = search_results
         if len(self.user_dict) == 0:
             # LDAP doesn't have this user
             return None
         else:
             self.create_unknown_user = True
+        self.user_dict = self.user_dict[0]
+        username = self.user_dict["username"]
 
-        # Django 3.2 doesn't run configure_user() for each user
-        return self.configure_user(request, super().authenticate(request, remote_user))
+        # Copy-pasted from https://github.com/django/django/blob/stable/3.2.x/django/contrib/auth/backends.py#L200C9-L211C66
+        if self.create_unknown_user:
+            user, created = UserModel._default_manager.get_or_create(**{
+                UserModel.USERNAME_FIELD: username
+            })
+            if created:
+                user = self.configure_user(request, user)
+        else:
+            try:
+                user = UserModel._default_manager.get_by_natural_key(username)
+            except UserModel.DoesNotExist:
+                pass
+        user = self.configure_user(request, user)
+        return user
 
     def clean_username(self, username):
-        return username.split("@")[0]
+        return username
+        # return username.split("@")[0]
 
     def configure_user(self, request, user, created=True):
-        ud = self.user_dict[0]
+        ud = self.user_dict
         user.first_name = ud["first_name"]
         user.last_name = ud["last_name"]
         user.email = ud["email"]
